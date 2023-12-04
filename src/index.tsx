e08bb3bd398cf0f0b6234eb6b094d8d98cb76df3
Sergey Maximyuk <m4ximyuk@gmail.com>
2023-12-04 23:51:05 +0300

    bugfix: add create root for app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import "./fonts/fonts.css";
 import "./components/ui/common.css";
 import "./components/ui/box.css";
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
